fadfba766648764bf093ca0cc446179f89255425
Jeffrey Kantor <kantor.1@nd.edu>
2022-02-08 23:01:51 -0500

    fix type

--- _build/jupyter_execute/notebooks/01/03_heat-transfer-model.py
+++ _build/jupyter_execute/notebooks/01/03_heat-transfer-model.py
@@ -22,14 +22,14 @@
 # 
 # where $C$ is a constant, $Re$ is the Reynold's number that is proportional to flowrate, and $Pr$ is the Prandtl number determined by fluid properties.
 # 
-# Experimentally, consider a set of values for $U$ determined by varying $\dot{m}_h$ and $\dot{m}_c$ over range of values. Because Reynold's number is proportional to flowrate, we can propose a model
+# Experimentally, consider a set of values for $U$ determined by varying $\dot{q}_h$ and $\dot{q}_c$ over range of values. Because Reynold's number is proportional to flowrate, we can propose a model
 # 
-# $$\frac{1}{U} = R_{tube} + r_h \dot{m}_h^{-0.8} + r_c \dot{m}_c^{-0.8}$$
+# $$\frac{1}{U} = R_{tube} + r_h \dot{q}_h^{-0.8} + r_c \dot{q}_c^{-0.8}$$
 # 
 # This suggests a regression procedure:
 # 
-# * Plot $\frac{1}{U}$ as a function of $\dot{m}_h^{-0.8}$ for fixed values of $\dot{m}_c$. Esitmate $r_h$ from the slope.
-# * Plot $\frac{1}{U}$ as a function of $\dot{m}_c^{-0.8}$ for fixed values of $\dot{m}_h$. Estimate r_c from the slope.
+# * Plot $\frac{1}{U}$ as a function of $\dot{q}_h^{-0.8}$ for fixed values of $\dot{q}_c$. Esitmate $r_h$ from the slope.
+# * Plot $\frac{1}{U}$ as a function of $\dot{q}_c^{-0.8}$ for fixed values of $\dot{q}_h$. Estimate r_c from the slope.
 # * Use the data and estimates of $r_h$ and $r_c$ to estimate $R_{tube}$.
 # 
 
